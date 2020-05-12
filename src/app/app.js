faf0900a17132742368aeb1bd0d2688dd011a205
Ally Jarjour <allyjarjour@gmail.com>
2020-05-12 14:55:14 -0600

    Add login form and input validation

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,32 @@
-import React from 'react';
+
 import './app.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import React, { Component } from 'react'
+import SignIn from '../sign-in/sign-in';
+
+export class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       name: "",
+       email: "",
+       purpose: ""
+    }
+  }
+
+  updateLogin = info => {
+    this.setState(info);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <SignIn updateLogin={this.updateLogin} />
+        {this.state.name && <p>{this.state.name}</p>}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
