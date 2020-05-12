b173e7a493513a012613f62651a1ef3dc324c2ab
Ally Jarjour <allyjarjour@gmail.com>
2020-05-12 17:46:39 -0600

    Style the login page and add images to public folder

--- src/sign-in/sign-in.js
+++ src/sign-in/sign-in.js
@@ -26,34 +26,37 @@ export class SignIn extends Component {
     
     render() {
         return (
-            <form className="Sign-In">
-                <label for="name">Name</label>
-                <input className={this.nameClass} type="text" name="name" onChange={this.change} value={this.state.name} />
-                <label for="email">Email</label>
-                <input className={this.emailClass} type="text" name="email" onChange={this.change} value={this.state.email} />
-                <select className={this.purposeClass} name="purpose" onChange={this.change}>
-                    <option disabled selected value>Choose one</option>
-                    <option value="vacation">Vacation</option>
-                    <option value="business">Business</option>
-                    <option value="other">Other</option>
-                </select>
-                <button onClick={e => {
-                    e.preventDefault(); 
-                    let allFilled = true;
-                    // check if inputs are filled
-                    for (const key in this.state) {
-                        if (!this.state[key]) {
-                            this[key + "Class"] = "error";
-                            allFilled = false;
-                        } else {
-                            this[key + "Class"] = "";
+            <div className="Sign-In-page">
+                <h1>vrad</h1>
+                <form className="Sign-In-form">
+                    <label for="name">Name</label>
+                    <input className={this.nameClass} type="text" name="name" onChange={this.change} value={this.state.name} placeholder="Name" />
+                    <label for="email">Email</label>
+                    <input className={this.emailClass} type="text" name="email" onChange={this.change} value={this.state.email} placeholder="Email" />
+                    <select className={this.purposeClass} name="purpose" onChange={this.change}>
+                        <option disabled selected value>Choose one</option>
+                        <option value="vacation">Vacation</option>
+                        <option value="business">Business</option>
+                        <option value="other">Other</option>
+                    </select>
+                    <button onClick={e => {
+                        e.preventDefault(); 
+                        let allFilled = true;
+                        // check if inputs are filled
+                        for (const key in this.state) {
+                            if (!this.state[key]) {
+                                this[key + "Class"] = "error";
+                                allFilled = false;
+                            } else {
+                                this[key + "Class"] = "";
+                            }
                         }
-                    }
 
-                    if (allFilled) this.props.updateLogin(this.state);
-                    else this.forceUpdate();
-                }}>Sign In</button>
-            </form>
+                        if (allFilled) this.props.updateLogin(this.state);
+                        else this.forceUpdate();
+                    }}>Sign In</button>
+                </form>
+            </div>
         )
     }
 }
