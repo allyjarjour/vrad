b1fc10647c095a25338ba1b89890866693e4941f
Ally Jarjour <allyjarjour@gmail.com>
2020-05-16 15:04:44 -0600

    Make minor edits to expanded listing page

--- src/expanded-listing/expanded-listing.js
+++ src/expanded-listing/expanded-listing.js
@@ -7,7 +7,7 @@ export default class ExpandedListing extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            favorited: false
+            favorited: true
         }
     }
 
@@ -32,14 +32,6 @@ export default class ExpandedListing extends Component {
             superhost: details.superhost
         }))
     }
-
-    displayFeatures = () => {
-        debugger
-        if(this.state.features) {
-            console.log(this.state.features);
-            return this.state.features.map((feature) => (<p>{feature}</p>))
-        }
-    }
     
     render () {
         if (!this.props.listingID) {
@@ -53,10 +45,11 @@ export default class ExpandedListing extends Component {
             <section className="details-section">
                 <div className="listing-title-container" >
                     <h1>{this.state.name}</h1>
-                    <svg className="unfavorited" xmlns="http://www.w3.org/2000/svg" 
+                    {this.showFaveHeart}
+                    {!this.state.favorited ? <svg className="fave-heart" xmlns="http://www.w3.org/2000/svg" 
                         width="24" height="24" viewBox="0 0 24 24">
                         <path d="M6.28 3c3.236.001 4.973 3.491 5.72 5.031.75-1.547 2.469-5.021 5.726-5.021 2.058 0 4.274 1.309 4.274 4.182 0 3.442-4.744 7.851-10 13-5.258-5.151-10-9.559-10-13 0-2.676 1.965-4.193 4.28-4.192zm.001-2c-3.183 0-6.281 2.187-6.281 6.192 0 4.661 5.57 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-4.011-3.097-6.182-6.274-6.182-2.204 0-4.446 1.042-5.726 3.238-1.285-2.206-3.522-3.248-5.719-3.248z"/>
-                    </svg>
+                    </svg> : <svg className="fave-heart" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z"/></svg>}
                 </div>
                 <div className="tags" >
                     <p className="area-tag">{this.state.area}</p>
@@ -69,7 +62,8 @@ export default class ExpandedListing extends Component {
                 </div>
                 <div className="listing-details">
                     <div className="features" >
-                        <h2>Features</h2>
+                        <h2>Listing details:</h2>
+                        <p>${this.state.cost}/night</p>
                         <p>{this.state.beds} beds</p>
                         <p>{this.state.baths} baths</p>
                         {this.state.features && this.state.features.map((feature , i)=> <p key={i}>{feature}</p>)}
@@ -84,6 +78,10 @@ export default class ExpandedListing extends Component {
         }
     }
 }
+
+ExpandedListing.propTypes = {
+    listingID: PropTypes.string,
+  };
 
 
 
